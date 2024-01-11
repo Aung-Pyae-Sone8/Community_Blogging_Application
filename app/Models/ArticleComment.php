9ee114a9f5315e56da9b9e3bfc301a87542d688c
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-11 18:15:10 +0630

    prepare-migrations-and-models

---
+++ app/Models/ArticleComment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleComment extends Model
+{
+    use HasFactory;
+    protected $fillable = ['article_id', 'user_id', 'comment'];
+}
